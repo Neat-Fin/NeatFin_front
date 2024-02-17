de15df0797d12388f7dc5a766238f3c2c2b5edea
yelin <juno2020k@gmail.com>
2024-02-18 01:08:38 +0900

    footer내용추가

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,8 +1,19 @@
 import "../Styles/Footerstyle.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faYoutube } from '@fortawesome/free-brands-svg-icons';
+
 function Footer() {
   return (
     <footer>
+      <a className="footericon" href="https://github.com/Neat-Fin" target="_blank">
+        <FontAwesomeIcon icon={faGithub} /> 
+      </a>
+      <a className="footericon" href="https://www.youtube.com/" target="_blank">
+        <FontAwesomeIcon icon={faYoutube} /> 
+      </a>
+
       © Copyright 2024 Neatfin
+     
     </footer>
   );
 }
